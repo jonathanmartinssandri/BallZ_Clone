3c72c6e475efcc9b4be6d293e5d7e5bbdc5dd320
lhcopetti <lhcopetti@gmail.com>
2017-06-30 20:16:14 -0300

    INT: Creation and design of the main menu screen

--- BallzClone/app/src/main/java/ballzclone/copetti/com/ballzclone/MainActivity.java
+++ BallzClone/app/src/main/java/ballzclone/copetti/com/ballzclone/MainActivity.java
@@ -1,13 +1,30 @@
 package ballzclone.copetti.com.ballzclone;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RenderView renderView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        renderView = new RenderView(this);
+        setContentView(renderView);
     }
 }
---
+++ BallzClone/app/src/main/java/ballzclone/copetti/com/ballzclone/MainMenuActivity.kt
@@ -0,0 +1,12 @@
+package ballzclone.copetti.com.ballzclone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
