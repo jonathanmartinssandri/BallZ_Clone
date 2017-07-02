0bf8b61b211c2f460ffd487933055fd87aefc87a
lhcopetti <lhcopetti@gmail.com>
2017-07-02 16:33:16 -0300

    INT: Clicking on the 'Highest scores' will open the Hall of Fame view

---
+++ BallzClone/app/src/main/java/ballzclone/copetti/com/ballzclone/HighestScoreActivity.kt
@@ -0,0 +1,12 @@
+package ballzclone.copetti.com.ballzclone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HighestScoreActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_highest_score)
+    }
+}
--- BallzClone/app/src/main/java/ballzclone/copetti/com/ballzclone/MainMenuActivity.kt
+++ BallzClone/app/src/main/java/ballzclone/copetti/com/ballzclone/MainMenuActivity.kt
@@ -1,14 +1,21 @@
 package ballzclone.copetti.com.ballzclone
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class MainMenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+    }
+
+    fun btnOpenHighestScoresClicked(v: View) {
+        val intent = Intent(this, HighestScoreActivity::class.java)
+        startActivity(intent)
     }
 
     fun exitGame(v : View) {
