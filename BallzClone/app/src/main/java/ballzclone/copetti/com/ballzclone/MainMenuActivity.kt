7a608029feb27f9bb02d635d99415b452c2d411a
lhcopetti <lhcopetti@gmail.com>
2017-07-02 16:19:11 -0300

    INT: A click on the 'Quit Game' button will exit the app

--- BallzClone/app/src/main/java/ballzclone/copetti/com/ballzclone/MainMenuActivity.kt
+++ BallzClone/app/src/main/java/ballzclone/copetti/com/ballzclone/MainMenuActivity.kt
@@ -2,11 +2,16 @@ package ballzclone.copetti.com.ballzclone
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainMenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+    }
+
+    fun exitGame(v : View) {
+        finish();
     }
 }
