62ad9bc37717bab9f536e989197848c4846bc4e1
lhcopetti <lhcopetti@gmail.com>
2017-07-02 17:53:49 -0300

    INT: Programmatically calling RenderView inside LinearLayout

--- BallzClone/app/src/main/java/ballzclone/copetti/com/ballzclone/GameActivity.kt
+++ BallzClone/app/src/main/java/ballzclone/copetti/com/ballzclone/GameActivity.kt
@@ -3,12 +3,25 @@ package ballzclone.copetti.com.ballzclone
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 
 class GameActivity : FullScreenActivity() {
+
+    private var gameView: View? = null
+    private var gameLayout: ViewGroup? = null
+
+    private var renderView : RenderView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        gameView = findViewById(R.id.mainGameView)
+        gameLayout = findViewById(R.id.gameViewLayout) as ViewGroup
+
+        var renderView: RenderView = RenderView(this)
+        renderView.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        gameLayout!!.addView(renderView)
     }
 
     fun btnPauseGameClicked(v : View)
