c70792608d57349e06fcf6fb5c95df7f4ecf1677
lhcopetti <lhcopetti@gmail.com>
2017-07-02 17:03:39 -0300

    INT: Clicking on the 'PLAY!' will open the game view

---
+++ BallzClone/app/src/main/java/ballzclone/copetti/com/ballzclone/GameActivity.kt
@@ -0,0 +1,18 @@
+package ballzclone.copetti.com.ballzclone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class GameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+
+    fun btnPauseGameClicked(v : View)
+    {
+
+    }
+}
--- BallzClone/app/src/main/java/ballzclone/copetti/com/ballzclone/MainMenuActivity.kt
+++ BallzClone/app/src/main/java/ballzclone/copetti/com/ballzclone/MainMenuActivity.kt
@@ -5,12 +5,22 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainMenuActivity : AppCompatActivity() {
+
+    private var bestOverall : TextView? = null
+    private var currentScore: TextView? = null
+    private var currentCoinValue: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+    }
+
+    fun btnOpenGameClicked(v: View) {
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
     }
 
     fun btnOpenHighestScoresClicked(v: View) {
