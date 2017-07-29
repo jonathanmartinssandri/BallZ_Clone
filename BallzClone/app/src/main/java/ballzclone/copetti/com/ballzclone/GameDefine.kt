18512e1855b70de637219e72da0e1080cd515b9d
Luis Copetti <lhcopetti@gmail.com>
2017-07-28 23:57:01 -0300

    INT: Change of velocity defines and power up radius

--- BallzClone/app/src/main/java/ballzclone/copetti/com/ballzclone/GameDefine.kt
+++ BallzClone/app/src/main/java/ballzclone/copetti/com/ballzclone/GameDefine.kt
@@ -9,11 +9,11 @@ object GameDefine {
     val GAME_SCREEN_WIDTH = 240 * 2
     val GAME_SCREEN_HEIGHT = 320 * 2
 
-    val ball_velocity: Float = 250.0f
+    val ball_velocity: Float = 350.0f
     val grid_velocity = BZVector2f(0.0f, 100.0f)
 
     val ball_radius: Float = 15.0f
-    val ball_powerup_radius: Float = 15.0f
+    val ball_powerup_radius: Float = 25.0f
 
     val cannon_shooting_interval = 0.5f
 
